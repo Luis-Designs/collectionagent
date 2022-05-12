dbeb5d58728abadb157a8537ea8a1e1f942b9c48
Luis Designs <luislozano120xd@gmail.com>
2022-05-12 11:53:44 -0500

    Start

--- index.py
+++ index.py
@@ -1,3 +1,26 @@
-slogan = "Somos Peru!"
+# Titulo: Tienda
+# Proposito: Vender y Almacenar productos!
+# Autor: Luis Designs
+# Fecha: Hoy
 
-print(slogan + str("Hurra!"))
+# name = input("\n Buen día, ingrese su nombre para iniciar: ")
+# # name = "Luis"
+# marco = "*" * 100   # Repite el * cien veces, para el marco
+# text = f"Welcome {name}, tenemos todo lo que necesitas!! 🦝"
+# # Se imprime el marco e incluye el nombre ingresado
+# print("\n" + marco)
+# print("*{:^97}*".format(text))
+# print(marco + "\n\n")
+
+# 1. Listar Productos
+# 2. Agregar un producto
+# 2. Remover un producto
+# 3. Añadir a cuenta
+# 4. Retirar de la cuenta
+# 5. Cobrar
+
+# Validar datos vacios
+
+products = {{'id': 1, 'name': 'Coca cola', 'quantity': 20, 'price': 10.5}}
+
+print(products.items())
